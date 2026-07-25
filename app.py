1adf8d4d6184f19b2de7356e4cf299c4af6c7aca
unknown <ujwalacharya74@gmail.com>
2026-07-25 15:50:16 +0545

    Implement data-driven A* route optimization

--- app.py
+++ app.py
@@ -1,24 +1,16 @@
-from astar import Node, AStar
-
-hotel = Node("Hotel", 28.2095, 83.9602)
-
-phewa = Node("Phewa Lake", 28.2096, 83.9596)
-
-peace = Node("World Peace Pagoda", 28.2004, 83.9448)
-
-davis = Node("Davis Falls", 28.1937, 83.9591)
-
-hotel.add_neighbor(phewa, 2)
-
-phewa.add_neighbor(peace, 4)
-
-peace.add_neighbor(davis, 3)
+from astar import AStar
 
 astar = AStar()
 
-route = astar.search(
-    hotel,
-    davis
+astar.build_graph(
+    "data/attractions.csv",
+    neighbors=3
 )
 
+route = astar.search(
+    "Phewa Lake",
+    "Davis Falls"
+)
+
+print("Optimized Route")
 print(route)
